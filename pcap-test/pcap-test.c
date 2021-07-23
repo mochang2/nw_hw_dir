97551990ebcf118dde037af196a779d05da9f646
mochang2 <qjsrodksro@gmail.com>
2021-07-23 15:17:34 +0000

    revised pcap-test code

--- pcap-test/pcap-test.c
+++ pcap-test/pcap-test.c
@@ -2,13 +2,12 @@
 // https://gitlab.com/gilgil/sns/-/wikis/basic-header-analysis/basic-header-analysis
 // https://gitlab.com/gilgil/sns/-/wikis/pcap-programming/pcap-programming
 // https://gitlab.com/gilgil/sns/-/wikis/pcap-programming/report-pcap-test
-
 #include <pcap.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <libnet.h>     // apt install libnet-deb
 #include <netinet/in.h>
-#include <unistd.h>     // sleep
+//#include <unistd.h>     // sleep to capture slowly
 
 #define ETHERNET_HEADER_LEN 14
 #define PAYLOAD_PRINTING_BYTE 8
@@ -44,8 +43,8 @@ bool parse(Param* param, int argc, char* argv[]) {
 	return true;
 }
 
-bool Is_footer(const u_char* packet, struct pcap_pkthdr* header, u_int ip_size, u_int tcp_size){
-    for (unsigned int i = 0; i < header->caplen - tcp_size - ip_size - ETHERNET_HEADER_LEN; i++){
+bool Is_footer(const u_char* packet, u_int ip_size, u_int tcp_size, u_int iph_tcph_tcppayload_size){
+    for (unsigned int i = 0; i <  iph_tcph_tcppayload_size - tcp_size - ip_size - ETHERNET_HEADER_LEN; i++){ // caplen -> totlen
         if (i == ETHERNET_FOOTER_BYTE)
             return true;
         if(packet[i])
@@ -54,9 +53,7 @@ bool Is_footer(const u_char* packet, struct pcap_pkthdr* header, u_int ip_size, 
     return true;
 }
 
-void print_the_result(struct libnet_ethernet_hdr *L2_hdr, struct libnet_tcp_hdr *L4_hdr, struct payload *L5_to_L7, struct pcap_pkthdr *header,
-                      u_int ip_size, u_int tcp_size, u_int8_t *ip_src_into_8bit_array, u_int8_t* ip_dst_into_8bit_array, bool print_payload){
-    printf("============printing starts============\n");
+void print_src_mac(struct libnet_ethernet_hdr *L2_hdr){
     printf("src_mac: ");
     for (int i = 0; i < MAC_BYTE; i++){
         if (i == MAC_BYTE - 1){
@@ -65,6 +62,8 @@ void print_the_result(struct libnet_ethernet_hdr *L2_hdr, struct libnet_tcp_hdr 
         }
         printf("%02x:", L2_hdr->ether_shost[i]);
     }
+}
+void print_dst_mac(struct libnet_ethernet_hdr *L2_hdr){
     printf("dst_mac: ");
     for (int i = 0; i < MAC_BYTE; i++){
         if (i == MAC_BYTE - 1){
@@ -74,15 +73,18 @@ void print_the_result(struct libnet_ethernet_hdr *L2_hdr, struct libnet_tcp_hdr 
         printf("%02x:", L2_hdr->ether_dhost[i]);
     }
     printf("\n");
-
+}
+void print_src_ip(u_int8_t *ip_src_into_8bit_array){
     printf("src_ip: ");
-    for (int i = 0; i < IPv4_BYTE; i++){
+    for (int i = 0; i < IPv4_BYTE; i++){ // =>function
         if (i == IPv4_BYTE - 1){
             printf("%u\n", ip_src_into_8bit_array[i]);
             break;
         }
         printf("%u.", ip_src_into_8bit_array[i]);
     }
+}
+void print_dst_ip(u_int8_t *ip_dst_into_8bit_array){
     printf("dst_ip: ");
     for (int i = 0; i < IPv4_BYTE; i++){
         if (i == IPv4_BYTE - 1){
@@ -92,13 +94,19 @@ void print_the_result(struct libnet_ethernet_hdr *L2_hdr, struct libnet_tcp_hdr 
         printf("%u.", ip_dst_into_8bit_array[i]);
     }
     printf("\n");
-
-    printf("src_port: %u\n", htons(L4_hdr->th_sport));
-    printf("dst_port: %u\n\n", htons(L4_hdr->th_dport));
-
+}
+void print_src_port(struct libnet_tcp_hdr *L4_hdr){
+    printf("src_port: %u\n", ntohs(L4_hdr->th_sport));
+}
+void print_dst_port(struct libnet_tcp_hdr *L4_hdr){
+    printf("dst_port: %u\n\n", ntohs(L4_hdr->th_dport));
+}
+void print_payload_func(bool print_payload, struct payload *L5_to_L7, u_int ip_size, u_int tcp_size, u_int iph_tcph_tcppayload_size){
     if (print_payload){
         printf("Payload_data: ");
-        for (unsigned int i = 0; i < header->caplen - tcp_size - ip_size - ETHERNET_HEADER_LEN; i++){
+
+        // ip_hdr->totlen(1 byte unit) - ip_hdr_size - tcp_hdr->offset * 4 : payload length
+        for (unsigned int i = 0; i < iph_tcph_tcppayload_size - tcp_size - ip_size - ETHERNET_HEADER_LEN; i++){ //caplen -> totlen
             if(i == 8)
                 break;
 
@@ -106,6 +114,21 @@ void print_the_result(struct libnet_ethernet_hdr *L2_hdr, struct libnet_tcp_hdr 
         }
         printf("\n\n");
     }
+}
+
+void print_the_result(struct libnet_ethernet_hdr *L2_hdr, struct libnet_tcp_hdr *L4_hdr, struct payload *L5_to_L7, u_int ip_size, u_int tcp_size,
+                      u_int iph_tcph_tcppayload_size ,u_int8_t *ip_src_into_8bit_array, u_int8_t* ip_dst_into_8bit_array, bool print_payload){
+    printf("============printing starts============\n");
+    print_src_mac(L2_hdr);
+    print_dst_mac(L2_hdr);
+
+    print_src_ip(ip_src_into_8bit_array);
+    print_dst_ip(ip_dst_into_8bit_array);
+
+    print_src_port(L4_hdr);
+    print_dst_port(L4_hdr);
+
+    print_payload_func(print_payload, L5_to_L7, ip_size, tcp_size, iph_tcph_tcppayload_size);
 }
 
 int main(int argc, char* argv[]) {
@@ -136,6 +159,7 @@ int main(int argc, char* argv[]) {
         struct payload *L5_to_L7;
         u_int ip_size, tcp_size;            // In case that IP or TCP uses options, not fixed 20 bytes
         u_int8_t ip_src_into_8bit_array[4], ip_dst_into_8bit_array[4];
+        // u_int32_t is more efficient
         bool print_payload;
 
         // capture packets
@@ -150,12 +174,12 @@ int main(int argc, char* argv[]) {
         L2_hdr = (struct libnet_ethernet_hdr*)(packet);
 
         // check if the upper type is IPv4 or not then type cast
-        if (htons(L2_hdr->ether_type) == 0x0800){
+        if (ntohs(L2_hdr->ether_type) == ETHERTYPE_IP){ // ntoh == ntoh in this case, consider from what to what
             packet += ETHERNET_HEADER_LEN;
             L3_v4_hdr = (struct libnet_ipv4_hdr*)(packet);
             ip_size = (L3_v4_hdr->ip_hl) * 4;
-            L3_v4_hdr->ip_src.s_addr = htonl(L3_v4_hdr->ip_src.s_addr);
-            L3_v4_hdr->ip_dst.s_addr = htonl(L3_v4_hdr->ip_dst.s_addr);
+            L3_v4_hdr->ip_src.s_addr = ntohl(L3_v4_hdr->ip_src.s_addr);
+            L3_v4_hdr->ip_dst.s_addr = ntohl(L3_v4_hdr->ip_dst.s_addr);
             for (int i=0; i < 4 ; i++){
                 ip_src_into_8bit_array[i] = ((uint8_t*)&(L3_v4_hdr->ip_src.s_addr))[3-i];
                 ip_dst_into_8bit_array[i] = ((uint8_t*)&(L3_v4_hdr->ip_dst.s_addr))[3-i];
@@ -165,7 +189,7 @@ int main(int argc, char* argv[]) {
             continue;
 
         // check if the length of the IP is valid or not and if the upper protocol is TCP or not.
-        if (ip_size >= 20 && L3_v4_hdr->ip_p == 0x06){
+        if (ip_size >= 20 && ip_size <=60 && L3_v4_hdr->ip_p == IPPROTO_TCP){
             packet += ip_size;
             L4_hdr = (struct libnet_tcp_hdr*)(packet);
             tcp_size = (L4_hdr->th_off) * 4;
@@ -174,13 +198,13 @@ int main(int argc, char* argv[]) {
             continue;
 
         // check if the lengh of the TCP is valid of not and if the remainder is the part of the Ethernet or not
-        if (tcp_size >= 20){
+        if (tcp_size >= 20 && tcp_size <= 60){
             packet += tcp_size;
-            print_payload = !Is_footer(packet, header, ip_size, tcp_size); // if not footer, print payload
+            print_payload = !Is_footer(packet, ip_size, tcp_size, L3_v4_hdr->ip_len); // if not footer, print payload
             L5_to_L7 = (struct payload*)(packet);
 
             // print MAC, IP, port, payload
-            print_the_result(L2_hdr, L4_hdr, L5_to_L7, header, ip_size, tcp_size, ip_src_into_8bit_array, ip_dst_into_8bit_array, print_payload);
+            print_the_result(L2_hdr, L4_hdr, L5_to_L7, ip_size, tcp_size, L3_v4_hdr->ip_len, ip_src_into_8bit_array, ip_dst_into_8bit_array, print_payload);
         }
         else
             continue;
